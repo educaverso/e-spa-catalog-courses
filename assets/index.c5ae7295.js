a7af337063dd728af0aacea27b607bc2a7bc872a
thiago1fc3 <thiago@lar.ifce.edu.br>
2022-05-20 11:24:17 -0300

    Updates

--- assets/index.c5ae7295.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,c as s,o as a,a as o}from"./index.ce264bf1.js";const c={},t={class:"ev-road-map"},n=o("h1",null,"This is an ROAD MAP page",-1),_=[n];function r(d,i){return a(),s("div",t,_)}var f=e(c,[["render",r]]);export{f as default};
