28c8c8d26c83671dd440109330c1d1e478833d8b
thiago1fc3 <thiago@lar.ifce.edu.br>
2022-05-20 23:44:02 -0300

    Updates

---
+++ assets/index.9b37dbc8.js
@@ -0,0 +1,1 @@
+import{_ as e,c as s,o as c,a as o}from"./index.0755a319.js";const t={},a={class:"ev-courses"},n=o("h1",null,"This is an COURSES page",-1),_=[n];function r(i,d){return c(),s("div",a,_)}var f=e(t,[["render",r]]);export{f as default};
