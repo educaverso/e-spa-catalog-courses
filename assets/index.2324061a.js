0aaf571e0b51906ba7c75b894bc481c2020f9304
thiago1fc3 <thiago@lar.ifce.edu.br>
2022-05-25 08:56:59 -0300

    Updates

---
+++ assets/index.2324061a.js
@@ -0,0 +1,1 @@
+import{_ as e,c as s,o,a as t}from"./index.751f5ed4.js";const c={},a={class:"ev-top"},n=t("h1",null,"This is an TOP page",-1),_=[n];function r(i,d){return o(),s("div",a,_)}var f=e(c,[["render",r]]);export{f as default};
