df03a052151ed6232899e0571890a19454418113
Erisnilton Freitas <erisniltongf@gmail.com>
2022-10-24 09:45:41 -0300

    feat: create component article

---
+++ src/infra/web/_components/article/article.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import type { PropType } from "vue";
+import type { TextContent } from "../course/modules/ModuleItem.vue";
+
+defineProps({
+  item: { type: Object as PropType<TextContent>, required: true },
+});
+</script>
+
+<template>
+  <p>{{ item.content }}</p>
+</template>
